bfd607ac603760e816537e57e4fdb1b0d05de47b
Björn Sjögren <bjorn.sjogren@technia.com>
2020-12-16 19:30:21 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Table from './Table'
+import Form from './Form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    state = {
+        characters: [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+              },
+              {
+                name: 'Mac',
+                job: 'Bouncer',
+              },
+              {
+                name: 'Dee',
+                job: 'Aspring actress',
+              },
+              {
+                name: 'Dennis',
+                job: 'Bartender',
+              },            
+        ],
+    };
 
-export default App;
+    render() {
+        const { characters } = this.state
+    
+    
+
+      return (
+        <div className="App container">
+          <h3>Hello, React!</h3>
+          <br/>          
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
+          <br/>
+          <Form handleSubmit={this.handleSubmit}/>
+        </div>
+      )
+    }
+
+    removeCharacter = index => {
+        const { characters } = this.state
+      
+        this.setState({
+          characters: characters.filter((character, i) => {
+            return i !== index
+          }),
+        })
+      }
+
+
+      handleSubmit = character => {
+        this.setState({ characters: [...this.state.characters, character] })
+      }
+  }
+
+  export default App
